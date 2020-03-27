72ae744a4f22e6e3cadb7c36478d0434a824e9ae
Matías Barrios <matias.barriosn@gmail.com>
2020-03-27 14:31:54 -0300

    feat: controllers syntax cleaned

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import { debugControllers } from '../config/debug'
+// import { debugControllers } from '../config/debug'
 import Logger from '../config/logger'
 import Deleted from '../models/deleted'
 import Stories from '../models/stories'
@@ -8,7 +8,6 @@ const fetch = async (request, response) => {
   try {
     let deleted = await Deleted.find({}, { _id: 0, story_id: 1 }).lean()
     deleted = deleted.map((del) => del.story_id)
-    debugControllers('deleted: \n', deleted)
     // fetch specific data
     const projection = {
       _id: 0,
@@ -23,9 +22,7 @@ const fetch = async (request, response) => {
     const stories = await Stories.find({ story_id: { $nin: deleted } }, projection)
     // Sort by created_at_i
     stories.sort((a, b) => b.created_at_i - a.created_at_i)
-    debugControllers('stories: \n', stories)
     response.success(stories)
-    // response.success(true)
   } catch (error) {
     Logger.error(error)
     response.error(error, 500)
@@ -35,7 +32,6 @@ const fetch = async (request, response) => {
 const del = async (request, response) => {
   try {
     const { storyId } = request.body
-    debugControllers('storyId', storyId)
     const deleted = { story_id: storyId }
     await Deleted.updateOne(deleted, deleted, { upsert: true })
     response.success({ deleted: storyId })
