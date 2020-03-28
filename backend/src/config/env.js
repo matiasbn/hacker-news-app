02e935bcd732940ec2fb088d2023c7ec1edce5eb
Matías Barrios <matias.barriosn@gmail.com>
2020-03-27 21:28:24 -0300

    feat: ignoring .env checking if it is docker

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -5,12 +5,13 @@ import Joi from '@hapi/joi'
 const environmentSchema = (environment) => {
   const joiString = Joi.string()
   const joiNumber = Joi.number()
-  return {
-    NODE_ENV: joiString.required().valid('development', 'production', 'test').default('development'),
-    MONGO_URI: joiString.required(),
-    APP_PORT: joiNumber.required(),
-    ...environment === 'main' && { DATABASE_NAME: joiString.required() },
-  }
+  return process.env.NODE_ENV !== 'production:docker'
+    ? {
+      NODE_ENV: joiString.required().valid('development', 'production', 'test').default('development'),
+      MONGO_URI: joiString.required(),
+      APP_PORT: joiNumber.required(),
+      ...environment === 'main' && { DATABASE_NAME: joiString.required() },
+    } : {}
 }
 
 dotenv.config({
